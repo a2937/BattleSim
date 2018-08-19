2647a9d993f8411a51891ffb129c672d7bcc4e40
Aaron <a.rcottrill521@gmail.com>
2018-08-19 11:33:54 -0400

    Add project files.

---
+++ BattleSim/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BattleSim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
